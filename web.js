7b1bf413fff60a278abc6aa8c5ebb6b9ed4bca8b
josecarmonagz <jose.carmonagz@gmail.com>
2013-08-14 22:42:32 +0000

    Initial Commmit

---
+++ web.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+app.use(express.logger());
+
+var fs = require('fs');
+
+var datos = fs.readFileSync('index.html', 'utf8');
+
+app.get('/', function(request, response) {
+  response.send(datos);
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
